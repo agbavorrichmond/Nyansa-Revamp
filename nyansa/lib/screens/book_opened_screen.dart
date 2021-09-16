36a941a47dc123f3353d6bb3929c4d239696f323
Eugene Asiedu <uginasiedu079@gmail.com>
2021-09-16 18:29:45 +0000

    Dictionary added

--- nyansa/lib/screens/book_opened_screen.dart
+++ nyansa/lib/screens/book_opened_screen.dart
@@ -1,8 +1,11 @@
 
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:nyansa/constants.dart';
 import 'package:nyansa/constants/colours.dart';
 
 class BookOpenedScreen extends StatefulWidget{
@@ -19,6 +22,9 @@ class _BookOpenedScreenState extends State<BookOpenedScreen> {
   double width;
   bool showControls = true;
 
+  bool playCLicked = false;
+  bool musicCLicked = false;
+  bool favoriteCLicked = false;
 
   @override
   void initState() {
@@ -64,6 +70,10 @@ class _BookOpenedScreenState extends State<BookOpenedScreen> {
        },
        child: Stack(
         children: [
+          Visibility(
+            visible: playCLicked,
+            child: dictionaryLayout(),
+          ),
           Align(
             alignment: Alignment.bottomCenter,
             child: Padding(
@@ -76,26 +86,59 @@ class _BookOpenedScreenState extends State<BookOpenedScreen> {
                   AnimatedOpacity(
                     opacity: showControls ? 1.0 : 0.0,
                     duration: const Duration(milliseconds: 300),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(35.0),
-                        color: ColorSys.white
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 17.0, horizontal: 54.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            SvgPicture.asset('assets/icons/music_icon.svg'),
-                            SizedBox(
-                              width: 70,
-                            ),
-                            SvgPicture.asset('assets/icons/play_icon.svg'),
-                            SizedBox(
-                              width: 70,
-                            ),
-                            SvgPicture.asset('assets/icons/like_icon.svg'),
-                          ],
+                    child: Visibility(
+                      visible: showControls,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(35.0),
+                          color: ColorSys.white
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 17.0, horizontal: 54.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              GestureDetector(
+                                onTap: (){
+                                  setState(() {
+                                    musicCLicked = !musicCLicked;
+                                  });
+                                },
+                                child: SvgPicture.asset(
+                                  'assets/icons/music_icon.svg',
+                                  color: musicCLicked ? kprimaryColor : ColorSys.gray,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 70,
+                              ),
+                              GestureDetector(
+                                onTap: (){
+                                  setState(() {
+                                    playCLicked = !playCLicked;
+                                  });
+                                },
+                                child: SvgPicture.asset(
+                                  'assets/icons/play_icon.svg',
+                                  color: playCLicked ? kprimaryColor : ColorSys.gray,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 70,
+                              ),
+                              GestureDetector(
+                                onTap: (){
+                                  setState(() {
+                                    favoriteCLicked = !playCLicked;
+                                  });
+                                },
+                                child: SvgPicture.asset(
+                                  'assets/icons/like_icon.svg',
+                                  color: favoriteCLicked ? kprimaryColor : ColorSys.gray,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
@@ -109,5 +152,108 @@ class _BookOpenedScreenState extends State<BookOpenedScreen> {
        ),
      ),
    );
+  }
+
+  Widget dictionaryTileLayout(){
+    return Opacity(
+      opacity: 0.4,
+      child: ClipRect(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+          child: Container(
+            width: 150,
+            height: 60,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: ColorSys.dictionaryTileColour,
+            ),
+
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  Widget dictionaryLayout(){
+    return Padding(
+      padding: const EdgeInsets.only(left: 80.0, right: 80.0, top: 48.0, bottom: 100),
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap: (){
+
+                  },
+                  child: dictionaryTileLayout(),
+                ),
+                GestureDetector(
+                  onTap: (){
+
+                  },
+                  child: dictionaryTileLayout(),
+                ),
+                GestureDetector(
+                  onTap: (){
+
+                  },
+                  child: dictionaryTileLayout(),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap: (){
+
+                  },
+                  child: dictionaryTileLayout(),
+                ),
+                GestureDetector(
+                  onTap: (){
+
+                  },
+                  child: dictionaryTileLayout(),
+                ),
+                GestureDetector(
+                  onTap: (){
+
+                  },
+                  child: dictionaryTileLayout(),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap: (){
+
+                  },
+                  child: dictionaryTileLayout(),
+                ),
+                GestureDetector(
+                  onTap: (){
+
+                  },
+                  child: dictionaryTileLayout(),
+                ),
+                GestureDetector(
+                  onTap: (){
+
+                  },
+                  child: dictionaryTileLayout(),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
